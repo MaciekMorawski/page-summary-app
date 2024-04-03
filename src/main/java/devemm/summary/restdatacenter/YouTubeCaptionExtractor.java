dcfccd72af39d6e7e62b92bfb1e3fbe735741a8d
mmorawski <maciek.morawski@gmail.com>
2024-04-03 12:36:55 +0200

    ref: url to properties

--- src/main/java/devemm/summary/restdatacenter/YouTubeCaptionExtractor.java
+++ src/main/java/devemm/summary/restdatacenter/YouTubeCaptionExtractor.java
@@ -1,17 +1,19 @@
 package devemm.summary.restdatacenter;
 
 import devemm.summary.pojo.SimpleJsonText;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestClient;
 
 @Repository
 public class RepositoryExtraction {
 
+public class YouTubeCaptionExtractor {
 
     private final RestClient restClient;
 
-    RepositoryExtraction() {
-        this.restClient = RestClient.create("http://localhost:5001");
+    YouTubeCaptionExtractor(@Value("${youtube.caption.extractor.url}") String url) {
+        restClient = RestClient.create(url);
     }
 
     public SimpleJsonText grabCaptions(String videoId) {
