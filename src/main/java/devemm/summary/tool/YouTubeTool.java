0d7e52990a876d4cff67770f6477292fe45a3ce1
mmorawski <maciek.morawski@gmail.com>
2024-04-05 11:45:55 +0200

    ref

--- src/main/java/devemm/summary/tool/YouTubeTool.java
+++ src/main/java/devemm/summary/tool/YouTubeTool.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class YouTubeTool {
 
-    public static String getVideoId(String url) {
-        String pattern = "(?<=watch\\?v=|/videos/|embed/|youtu.be/|/v/|/e/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#&?\\n]*";
+    private static final String pattern = "(?<=watch\\?v=|/videos/|embed/|youtu.be/|/v/|/e/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#&?\\n]*";
+    private static final Pattern compiledPattern = Pattern.compile(pattern);
 
-        Pattern compiledPattern = Pattern.compile(pattern);
+    public static String getVideoId(String url) {
         Matcher matcher = compiledPattern.matcher(url);
         if (matcher.find()) {
             return matcher.group();
