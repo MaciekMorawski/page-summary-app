5c68201219b9108c1f213693cf71e5a3e5aa3893
mmorawski <maciek.morawski@gmail.com>
2024-04-08 09:45:27 +0200

    Global exception handler

--- src/main/java/devemm/summary/summary/CtrlSumaryzacja.java
+++ src/main/java/devemm/summary/summary/CtrlSumaryzacja.java
@@ -4,7 +4,6 @@ import devemm.summary.pojo.SimpleJsonText;
 import devemm.summary.summary.grabber.TxtGrabber;
 import devemm.summary.tool.PathChooser;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
