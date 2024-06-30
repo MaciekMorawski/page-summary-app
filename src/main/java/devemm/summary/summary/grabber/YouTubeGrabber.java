d7cc12f94d4c5847801c395ae5da377624029e9b
maciek <maciek.morawski@gmail.com>
2024-06-30 19:12:48 +0200

    ref

--- src/main/java/devemm/summary/summary/grabber/YouTubeGrabber.java
+++ src/main/java/devemm/summary/summary/grabber/YouTubeGrabber.java
@@ -1,7 +1,7 @@
 package devemm.summary.summary.grabber;
 
 import devemm.summary.restdatacenter.youtube.ServiceYouTubeCaption;
-import devemm.summary.summary.TranslatorPL;
+import devemm.summary.summary.translator.TranslatorPL;
 import devemm.summary.tool.YouTubeTool;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Qualifier;
--- src/main/java/devemm/summary/summary/translator/TranslatorPL.java
+++ src/main/java/devemm/summary/summary/translator/TranslatorPL.java
@@ -1,4 +1,4 @@
-package devemm.summary.summary;
+package devemm.summary.summary.translator;
 
 
 import devemm.summary.grok.repository.RepositoryGroq;
@@ -20,21 +20,29 @@ public class TranslatorPL {
 
         List<NativeOpenAiChatDataModelIn.Message> messages = new NativeOpenAiChatDataModelIn().getMessages();
 
-        messages.add(nativeOpenAiChatDataModelIn.new Message(PromptRole.user, translateBewlowEnToPl(txtToSummarize)));
+        messages.add(nativeOpenAiChatDataModelIn.new Message(PromptRole.user, translateBelowEnToPl(txtToSummarize)));
 
-        nativeOpenAiChatDataModelIn.setModel("llama2-70b-4096");
-        nativeOpenAiChatDataModelIn.setMessages(messages);
-        nativeOpenAiChatDataModelIn.setTemperature(0.1);
-        nativeOpenAiChatDataModelIn.setMaxTokens(1024);
-        nativeOpenAiChatDataModelIn.setTopP(0);
-        nativeOpenAiChatDataModelIn.setStream(false);
-        nativeOpenAiChatDataModelIn.setStop(null);
+        setupModelEngine(nativeOpenAiChatDataModelIn);
+        setupModelMessages(nativeOpenAiChatDataModelIn, messages);
 
         return repositoryGroq.talk(nativeOpenAiChatDataModelIn);
 
     }
 
-    private String translateBewlowEnToPl(String txt) {
+    private void setupModelMessages(NativeOpenAiChatDataModelIn nativeOpenAiChatDataModelIn, List<NativeOpenAiChatDataModelIn.Message> messages) {
+        nativeOpenAiChatDataModelIn.setMessages(messages);
+    }
+
+    private void setupModelEngine(NativeOpenAiChatDataModelIn nativeOpenAiChatDataModelIn) {
+        nativeOpenAiChatDataModelIn.setModel("llama2-70b-4096");
+        nativeOpenAiChatDataModelIn.setTemperature(0.1);
+        nativeOpenAiChatDataModelIn.setMaxTokens(1024);
+        nativeOpenAiChatDataModelIn.setTopP(0);
+        nativeOpenAiChatDataModelIn.setStream(false);
+        nativeOpenAiChatDataModelIn.setStop(null);
+    }
+
+    private String translateBelowEnToPl(String txt) {
         return "Przetłumacz  tekst  na jezyk polski: \n" + txt;
     }
 }
