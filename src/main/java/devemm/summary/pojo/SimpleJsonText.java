97c43a80a7a7c5e4fa7d328adea53fe8ad5884bf
maciek <maciek.morawski@gmail.com>
2024-07-01 21:15:06 +0200

    ref: class to record

--- src/main/java/devemm/summary/pojo/SimpleJsonText.java
+++ src/main/java/devemm/summary/pojo/SimpleJsonText.java
@@ -4,23 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class SimpleJsonText
-{
-    @JsonProperty("txt")
-    @NotBlank(message = "txt cannot be blank")
-    @NotNull(message = "txt cannot be null")
-    @Pattern(regexp = "^(https?):\\/\\/[^\\s/$.?#].[^\\s]*$", message = "txt is not valid url")
-    private String txt;
-
-    SimpleJsonText() {
-    }
-
-    public SimpleJsonText(String talk) {
-        this.txt = talk;
-    }
+public record SimpleJsonText(
+        @JsonProperty("txt") @NotBlank(message = "txt cannot be blank") @NotNull(message = "txt cannot be null") @Pattern(regexp = "^(https?):\\/\\/[^\\s/$.?#].[^\\s]*$", message = "txt is not valid url") String txt) {
 }
