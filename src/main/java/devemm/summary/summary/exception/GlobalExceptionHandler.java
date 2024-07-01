2729c1b004e14b2395f32a8c26a57213059277f9
maciek <maciek.morawski@gmail.com>
2024-07-01 19:37:31 +0200

    ref

--- src/main/java/devemm/summary/summary/exception/GlobalExceptionHandler.java
+++ src/main/java/devemm/summary/summary/exception/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package devemm.summary.summary.exception;
 
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +18,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseError);
     }
 
-    @Getter
-    @RequiredArgsConstructor
-    static class ResponseError {
-        private final String error;
-        private final String message;
+
+    record ResponseError(String error, String message) {
     }
 }
