c03e55e37bbdfac323a60bb57f10950d647c966f
maciek <maciek.morawski@gmail.com>
2024-07-08 20:47:32 +0200

    logger add

--- src/main/java/devemm/summary/app/serv/grabber/OthersGrabber.java
+++ src/main/java/devemm/summary/app/serv/grabber/OthersGrabber.java
@@ -3,11 +3,13 @@ package devemm.summary.app.serv.grabber;
 
 import devemm.summary.restdatacenter.others.OtherService;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("OtherGrabber")
+@Log4j2
 public class OthersGrabber implements TxtGrabber{
 
     private final OtherService serviceOthers;
@@ -23,6 +25,7 @@ public class OthersGrabber implements TxtGrabber{
 
     @Override
     public String getTxtFromUrl(String url) {
+        log.debug("Get txt from url: {}", url);
         return serviceOthers.grabOtherTxtUsingUrl(url);
     }
 }
