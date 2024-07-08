5706e795ee3dbc381d9cb1da8b8c411b099ad858
maciek <maciek.morawski@gmail.com>
2024-07-08 20:44:34 +0200

    new Groq model

--- src/main/java/devemm/summary/ai/grok/GroqModel.java
+++ src/main/java/devemm/summary/ai/grok/GroqModel.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum GroqModel {
-    LLAMA_3_70_B_8192("llama3-70b-8192")
+    LLAMA_3_70_B_8192("llama3-70b-8192"),
+    GEMMA2_9B_IT("gemma2-9b-it"),
     ;
 
     private final String modelName;
